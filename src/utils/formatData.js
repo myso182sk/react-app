bfdc524805b7aaccfa6d5511ecdac4185d658be3
Michal <myso@tridaz.eu>
2024-05-01 21:11:11 +0200

    Added function for URL formatting

--- src/utils/formatData.js
+++ src/utils/formatData.js
@@ -1,3 +1,7 @@
 export function formatData(string) {
     return JSON.stringify( string, null, 2 ).replace(/["{[,\}\]]/g, "");
 }
+
+export function formatURL(string) {
+    return string.replace( "//uploads", "/uploads" ); 
+}
