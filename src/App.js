31b55cfcd09505a581d5160b9c4cab6fcf76e5d1
Michal <myso@tridaz.eu>
2024-05-01 20:44:55 +0200

    Replaced Components by Router

--- src/App.js
+++ src/App.js
@@ -1,32 +1,34 @@
 import React, { useState, useEffect } from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { useLocation } from 'react-router-dom';
 
+// Import logo (demo)
 import logo from './logo.svg';
 
+// Import css
 import './App.css';
 import './scss/main.css';
 import './scss/skin.css';
 
 // Import components
-import TopHeader from './components/TopHeader';
-import Header from './components/Header';
-import Carousel from './components/Carousel';
-import PlainText from './components/PlainText';
-import PricingTable from './components/PricingTable';
-import Footer from './components/Footer';
-import BottomFooter from './components/BottomFooter';
+import Header from './layout/Header';
+import MainContent from './layout/MainContent';
+import NotFound from './layout/NotFound';
+import Footer from './layout/Footer';
 
-function App() {
+export default function App() {  
+  const myPathname = window.location.pathname;
   return (
     <body id="wrapper">
-      <TopHeader />
-      <Header />
-      <Carousel />
-      <PlainText />
-      <PricingTable />
-      <Footer />
-      <BottomFooter />
+      <Router>
+        <Header />
+        <Routes>
+          <Route exact path="/" element={<MainContent />} />
+          <Route path="/:slug" element={<MainContent pathname={myPathname} />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+        <Footer />
+      </Router>
     </body>
   );
 }
-
-export default App;
